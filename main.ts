1774b855ae1645000f588bef60b217814c27ea14
fgygfdfg <115881073+fgygfdfg@users.noreply.github.com>
2022-10-15 22:10:22 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,55 @@
+let head = 0
+input.onGesture(Gesture.Shake, function () {
+    head = randint(1, 6)
+})
+basic.forever(function () {
+    if (1 == head) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (2 == head) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (3 == head) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (4 == head) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (5 == head) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (6 == head) {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            `)
+    }
+})
